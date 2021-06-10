04b77d17359770059bc3438c3a109c02cabba150
Emine İnan <emineinanx@gmail.com>
2021-06-10 18:05:31 +0300

    Set tab bar

--- app/src/main/java/com/example/findremotejob/fragments/MainFragment.kt
+++ app/src/main/java/com/example/findremotejob/fragments/MainFragment.kt
@@ -6,16 +6,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.findremotejob.R
+import com.example.findremotejob.databinding.FragmentMainBinding
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems
 
 class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpTabBar()
+    }
+
+    private fun setUpTabBar() {
+        val adapter = FragmentPagerItemAdapter(
+            childFragmentManager,
+            FragmentPagerItems.with(activity)
+                .add("Jobs", RemoteJobsFragment::class.java)
+                .add("Search", SearchJobFragment::class.java)
+                .add("Saved Jobs", SavedJobsFragment::class.java)
+                .create()
+        )
+
+        binding.viewpager.adapter = adapter
+        binding.viewPagerTab.setViewPager(binding.viewpager)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
